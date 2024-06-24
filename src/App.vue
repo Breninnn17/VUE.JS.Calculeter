d8b7fd4833e01d86c61fc7885e253100d6b503cd
Breno Neves <Brenonevessantos2014@hotmail.com>
2024-06-24 16:42:40 -0300

    frist commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,68 @@
 <script setup>
+import { reactive } from "vue";
+import Form from "./componetes/Form.vue";
 
+const estado = reactive({
+    filtro: "multiplicacao",
+    numero1: 0,
+    numero2: 0,
+});
+
+const multiplicacao = ({ numero1, numero2 } = estado) => {
+  return numero1 * numero2;
+};
+
+const adicao = ({ numero1, numero2 } = estado) => {
+    return Number(numero1 + numero2);
+};
+
+const subtracao = ({ numero1, numero2 } = estado) => {
+    return numero1 - numero2;
+};
+
+const divisao = ({ numero1, numero2 } = estado) => {
+    return numero1 / numero2;
+};
+
+const matematica = () => {
+    const { numero1, numero2, filtro } = estado;
+
+if ((numero1, numero2)) {
+    switch (filtro) {
+        case "multiplicacao":
+        return multiplicacao();
+        case "adicao":
+        return adicao();
+        case "subtracao":
+        return subtracao();
+        case "divisao":
+        return divisao();
+    }
+}
+};
 </script>
 
 <template>
+    <div class="container">
+        <header>
+        <h1>Operações Matemáticas</h1>
+        <p>Digite os Numeros</p>
+    </header>
+    <Form
+        :trocar-filtro="(event) => (estado.filtro = event.target.value)"
+        :valor-numero1="(event) => (estado.numero1 = Number(event.target.value))"
+        :valor-numero2="(event) => (estado.numero2 = Number(event.target.value))"
+    />
 
+    <p>O resultado é {{ matematica() }}</p>
+</div>
 </template>
 
 <style scoped>
 
+#app{
+    display: flex;
+    align-items: center;
+    justify-content: center
+}
 </style>
---
+++ src/componetes/Form.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(["valorNumero1", "valorNumero2", "trocarFiltro"]);
+</script>
+
+    <template>
+    <form>
+        <div >
+            <input @keyup="props.valorNumero1" class="form-control" type="number" />
+            <input @keyup="props.valorNumero2" class="form-control" type="number" />
+        </div>
+        <div class="col">
+            <select @change="props.trocarFiltro" class="form-control text-center">
+            <option value="multiplicacao">Multiplicação</option>
+            <option value="adicao">Adição</option>
+            <option value="subtracao">Subtração</option>
+            <option value="divisao">Divisão</option>
+            </select>
+        </div>
+    </form>
+    </template>
